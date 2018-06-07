aa42313353d9d1f1725a77d3c4f7541a87de2b41
Cevi Herdian <itsmecevi@gmail.com>
2018-06-07 10:22:44 +0200

    Add files via upload

---
+++ pointer-value/app.R
@@ -0,0 +1,19 @@
+library(shiny)
+library(astsa)
+
+ui <- basicPage(
+  plotOutput("plot1", click = "plot_click"),
+  verbatimTextOutput("info")
+)
+
+server <- function(input, output) {
+  output$plot1 <- renderPlot({
+    plot(lynx)
+  })
+  
+  output$info <- renderText({
+    paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y)
+  })
+}
+
+shinyApp(ui, server)
